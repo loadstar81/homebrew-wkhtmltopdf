c4ecfe530ea087c57031fd7c681e813c8f7ae106
Tom Mast <tom.mast@greenlight.guru>
2019-01-22 13:33:03 -0500

    Added 0.12.2.1

---
+++ Casks/wkhtmltopdf01221.rb
@@ -0,0 +1,21 @@
+cask "wkhtmltopdf01221" do
+  name "wkhtmltopdf"
+  version "0.12.2.1"
+  homepage "https://wkhtmltopdf.org"
+  sha256 "c2fd9b39182453ba9672f528e7a503928e51bc6a45c3117da06a5193af338d35"
+
+  url "https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.2.1/wkhtmltox-0.12.2.1_osx-cocoa-x86-64.pkg"
+
+  pkg "wkhtmltox-0.12.2.1_osx-cocoa-x86-64.pkg"
+
+  uninstall pkgutil: "org.wkhtmltopdf.wkhtmltox",
+            delete: [
+                     "/usr/local/include/wkhtmltox",
+                     "/usr/local/lib/libwkhtmltox.dylib",
+                     "/usr/local/lib/libwkhtmltox.0.dylib",
+                     "/usr/local/lib/libwkhtmltox.0.12.dylib",
+                     "/usr/local/lib/libwkhtmltox.0.12.2.1.dylib",
+                     "/usr/local/bin/wkhtmltoimage",
+                     "/usr/local/bin/wkhtmltopdf",
+                    ]
+end
